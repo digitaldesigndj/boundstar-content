d8b3f656031271cb96825651684bba9ffc660c4c
Taylor Young <tdy721@gmail.com>
2014-01-31 00:06:24 -0600

    background change finish

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 // Background
-$.backstretch("assets/img/bg.jpg", {speed: 1000});
+$.backstretch("assets/img/bg.png", {speed: 1000});
 // Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
